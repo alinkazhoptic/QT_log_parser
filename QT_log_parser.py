1ea10501d65285e3186b5c1a0f53ea680c933fdc
alinkazhoptic <borisova_alina_95@mail.ru>
2022-06-30 16:40:10 +0300

    add ID column in optical_log

--- QT_log_parser.py
+++ QT_log_parser.py
@@ -38,33 +38,55 @@ print(len(loglines), 'lines successfully read')
 
 #Запись всех линий (строк) с оптическим логом в один массив qkdmetriclines
 qkdmetriclines = []
-num_of_req_date = 0  # счетчик строк с требуемой датой для проверки наличия лога за эту дату
+parse_phrase_sent = "qkdom"
+num_of_req_str_sent = 0  # счетчик строк, содержащих parse_phrase_1
+
+parse_phrase_received = "QK_GENERATION_LOG"
+num_of_req_str_received = 0  # счетчик строк, содержащих parse_phrase_2
+
 for line in loglines:
-#    if (line.find(Date_of_interest) >= 0) and ( (line.find('qkdom') >= 0) and (line.find('Metrics:') >= 0) ):
-    #if  ((line.find('qkdom') >= 0) and (line.find('Metrics:') >= 0)):
-    if (line.find('QK_GENERATION_LOG') >= 0) :
-        num_of_req_date = num_of_req_date + 1
+    if (line.find(parse_phrase_received) >= 0) :
+        num_of_req_str_received = num_of_req_str_received + 1
         if line.find('"QBerr"') > 0:
             qkdmetriclines.append(line)
             # print(line) # просто вывод всех полезных строк лог-файла
         else:
-            print('There is no QBER data at this series')
+             print('There is no QBER data for this series')
 
-if num_of_req_date == 0:
-    print('\nRecuired date does not exist in this log-file')
+for line in loglines:
+    if ( (line.find(parse_phrase_sent) >= 0) and (line.find('Metrics:') >= 0) ):
+        num_of_req_str_sent = num_of_req_str_sent + 1
+        if line.find('"QBerr"') > 0:
+            if num_of_req_str_received == 0:
+                qkdmetriclines.append(line)
+        else:
+            print('There is no QBER data for this series')
+
+if (num_of_req_str_sent != num_of_req_str_received): # проверка равенства числа полученных УК логов с количеством отправленных
+    print('The number of sent logs is not equal to the number of received logs')
+
+if (qkdmetriclines != []):
+    print('Parsing completed successfully!')
+    if (num_of_req_str_sent == 0) :
+        print ('Warning: There is no "' + parse_phrase_sent + '" in this log')
+    if (num_of_req_str_received == 0) :
+        print ('Warning: There is no "' + parse_phrase_received + '" in this log')
+    # print(len(qkdmetriclines), 'series lines')
+
+elif ( (num_of_req_str_sent == 0) & (num_of_req_str_received == 0) ):
+    print('\nRecuired "' + parse_phrase_received + '" or "' + parse_phrase_received + '" does not exist in this log-file')
     print('Close after 10 seconds')
     time.sleep(10)
     raise SystemExit()
 
-print(len(qkdmetriclines), 'series lines')
 
-
+\
 # *** Выделение временной метки из массива линий qkdmetriclines ***
 time_array = []
 date_array = []
 timestamp_array = []
 
-k = 0
+# k = 0
 for st in qkdmetriclines:
     search_result_datestamp = re.search(r'(?P<month>\b\w{3}\b)\W+(?P<day>\d+)', st)
     search_result_timestamp = re.search(r'(?P<hours>\d\d):(?P<minutes>\d\d):(?P<seconds>\d\d)', st)
@@ -83,7 +105,7 @@ for st in qkdmetriclines:
     timestamp_array.append( search_result_timestamp.group('hours') + ':' + search_result_timestamp.group('minutes') + ':' + search_result_timestamp.group('seconds') )
     time_array.append(seconds_total)
     # print('time [', k, ']:', time_array[k], 's')
-    k = k + 1
+    # k = k + 1
 
 # Смещение массива времени для счета относительно запуска системы
 time_start = time_array[0]
@@ -91,6 +113,18 @@ for i in range(len(time_array)):
     time_array[i] = time_array[i] - time_start
     print('Relative time after start [', i, ']:', time_array[i], 's')
 print('\nlength of time array = ', len(time_array), '\n')
+
+# *** Выделение Alice ID
+alice_id_array = []
+print('IDs of QSS Points:')
+if num_of_req_str_received > 0:
+    for st in qkdmetriclines:
+        search_result_idstamp = re.search(r'(?P<qk>QK_GENERATION_LOG).(?P<id>\w{10}).', st)
+        # print(search_result_idstamp)       #Вывод ID Алис, с которыми делались ключи
+        alice_id_value = search_result_idstamp.group('id')
+        alice_id_array.append(alice_id_value)
+num_of_ids = len(alice_id_array)
+# print ('num of found IDs = ' + str(num_of_ids) )
 
 
 # **************************************************************************
@@ -127,6 +161,8 @@ for x in range(N_QKD_series):
         json_all_data[x]['Time after start, s'] = time_array[x]  # Добавление шкалы времени в словарь
         json_all_data[x]['Date'] = date_array[x] # Добавление даты
         json_all_data[x]['Timestamp'] = timestamp_array[x] # Добавление абсолютного времени
+        if (num_of_ids > 0):
+            json_all_data[x]['Alice ID'] = alice_id_array[x]  # Добавление идентификатора QSS Point (Alice ID)
 
         print('json to dict str[', x, ']', json_all_data[x])
         json_all_keys = list( json_all_data[x].keys() )   # список ключей в текущем словаре
@@ -134,6 +170,11 @@ for x in range(N_QKD_series):
     else:
         json_all_data[x]['Time after start, s'] = time_array[x]  # Добавление шкалы времени в словарь
         json_all_data[x]['NumOfSeries'] = x  # Добавление номера серии в словарь
+        json_all_data[x]['Date'] = date_array[x]  # Добавление даты
+        json_all_data[x]['Timestamp'] = timestamp_array[x]  # Добавление абсолютного времени
+        if (num_of_ids > 0):
+            json_all_data[x]['Alice ID'] = alice_id_array[x]  # Добавление идентификатора QSS Point (Alice ID)
+
         print('No data in str[', x, ']')
 
 N_keys = len(json_all_keys)
